317caa93134f61c44d4f4ecce601ad00ab45c7e5
cgkw94 <colingohh@gmail.com>
2022-01-13 13:35:50 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,29 @@
+// Dependencies
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const Hotel = require("./models/hotel");
+const seedData = require("./models/Seed");
+
+const mongoURI = "mongodb://127.0.0.1:27017/hotel";
+connectDB(mongoURI);
+
+// Connect to Mongo
+mongoose.connect(
+  mongoURI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("The connection with mongod is established");
+  }
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/seed", async (req, res) => {
+  await Product.deleteMany({});
+  await Product.create(seedData);
+  res.send("Seed data created");
+});
 
 app.listen(5001);
