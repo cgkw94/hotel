a915986eb5e8d736aff73c4c21bbd8406ea393d9
ranie <ranieseah@gmail.com>
2022-01-14 16:48:15 +0800

    login route complete in react app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
+  // eslint-disable-next-line -- this is to "get rid" of the compilation warning that the state was assigned but never used.
+  const [userFeedback, setUserFeedback] = useState("");
+  // eslint-disable-next-line
+  const [userInfo, setUserInfo] = useState(0);
+  // eslint-disable-next-line
+  const [loggedIn, setLoggedIn] = useState(false);
+  const handleSubmit = () => {};
   return (
-    <form onSubmit={handleSubmit}>
-      <div>Feedback</div>
-      <input
-        type="text"
-        name="feedback"
-        onChange={(e) => setUserFeedback(e.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <Route exact path="/">
+        <a href="/login">
+          <button>Login / Sign Up</button>
+        </a>
+        <form onSubmit={handleSubmit}>
+          <div>Feedback</div>
+          <input
+            type="text"
+            name="feedback"
+            onChange={(e) => setUserFeedback(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </Route>
+      <Route path="/login">
+        <Login setUserInfo={setUserInfo} setLoggedIn={setLoggedIn} />
+      </Route>
+    </>
   );
 }
 
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        login
+        <br />
+        <form>
+          <input placeholder="email"></input>
+          <input placeholder="password"></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+        Sign Up
+        <br />
+        <form>
+          <input placeholder="username"></input>
+          <input placeholder="email"></input>
+          <input placeholder="password"></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
