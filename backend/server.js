229e03304469d9e79f3c12180334446d74c6d911
cgkw94 <colingohh@gmail.com>
2022-01-17 10:38:15 +0800

    post working

--- backend/server.js
+++ backend/server.js
@@ -89,14 +89,15 @@ app.post("/feedback/create", async (req, res) => {
 
   const newFeedback = req.body.feedback;
 
-  //for some reason cant update DB using this syntax but express can receive post
-  Hotel.updateOne(
-    //query
+  await Hotel.updateOne(
     { hotelId: 1 },
-    //update
     {
       $push: {
-        feedback: newFeedback,
+        feedback: {
+          username: newFeedback.username,
+          userRating: newFeedback.userRating,
+          userFeedback: newFeedback.userFeedback,
+        },
       },
     }
   );
@@ -107,66 +108,68 @@ app.post("/feedback/create", async (req, res) => {
 ////////////////////////////////////
 
 app.put("/hotel/:id", async (req, res) => {
-
-  let id = parseInt(req.params.id)
-  let inDate = req.body.inDate
-  let outDate = req.body.outDate
-  let userName = req.body.userName
-  let roomType = req.body.roomType
-  let roomSize = req.body.roomSize
+  let id = parseInt(req.params.id);
+  let inDate = req.body.inDate;
+  let outDate = req.body.outDate;
+  let userName = req.body.userName;
+  let roomType = req.body.roomType;
+  let roomSize = req.body.roomSize;
 
   const hotelDetails = await Hotel.findOne({
     hotelId: `${req.params.id}`,
   });
 
-  hotelDetails.userStayed.push(userName)
-  hotelDetails.rooms.push({inDate: inDate, outDate: outDate, roomType: roomType, roomSize: roomSize})
+  hotelDetails.userStayed.push(userName);
+  hotelDetails.rooms.push({
+    inDate: inDate,
+    outDate: outDate,
+    roomType: roomType,
+    roomSize: roomSize,
+  });
 
-  Hotel.updateOne({hotelId: id}, hotelDetails, function(err, res) {
+  Hotel.updateOne({ hotelId: id }, hotelDetails, function (err, res) {
     if (err) {
-      console.log(err)
-      res.status(500)
-      res.json({})
+      console.log(err);
+      res.status(500);
+      res.json({});
     } else {
-      console.log(res)
+      console.log(res);
     }
-  })
-  res.status(200)
-  res.json(hotelDetails)
-})
+  });
+  res.status(200);
+  res.json(hotelDetails);
+});
 
 ////////////////////////////////////
 // GET list of available hotels
 ////////////////////////////////////
 
 app.get("/hotel/", async (req, res) => {
+  let inDate = req.query.inDate;
+  let outDate = req.query.outDate;
+  let inDateUnix = Date(inDate);
+  let outDateUnix = Date(outDate);
+  let roomSize = req.query.roomSize;
+  let allHotels = await Hotel.find();
 
-  let inDate = req.query.inDate
-  let outDate = req.query.outDate
-  let inDateUnix = Date(inDate)
-  let outDateUnix = Date(outDate)
-  let roomSize = req.query.roomSize
-  let allHotels = await Hotel.find()
-  
-  allHotels = allHotels.filter(singleHotel => {
-  
-    let bookings = singleHotel.rooms.filter(room => room.maxPax === roomSize);
+  allHotels = allHotels.filter((singleHotel) => {
+    let bookings = singleHotel.rooms.filter((room) => room.maxPax === roomSize);
     // for each of the existing bookings, the logic below checks every booking to see if it overlaps with user's in and out dates
-    return bookings.every(booking => {
-      // Date.parse converts the in and out dates to unix for comparison
+    return (
+      bookings.every((booking) => {
+        // Date.parse converts the in and out dates to unix for comparison
 
-      let bookingStart = Date.parse(booking.inDate)
-      let bookingEnd = Date.parse(booking.outDate)
-  
-      return outDateUnix < bookingStart || inDateUnix > bookingEnd //the conditions to make sure it doesn't overlap
+        let bookingStart = Date.parse(booking.inDate);
+        let bookingEnd = Date.parse(booking.outDate);
 
-    }) && singleHotel.location === req.query.location
+        return outDateUnix < bookingStart || inDateUnix > bookingEnd; //the conditions to make sure it doesn't overlap
+      }) && singleHotel.location === req.query.location
+    );
+  });
 
-  })
-  
   // at this point, Hotels that's not available are filtered out already
-  res.json(allHotels)
-})
+  res.json(allHotels);
+});
 
 ////////////////////////////////////
 // Login to obtain Users Data
