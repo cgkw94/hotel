6d08bb7157d0c8fe8b7bff127eb9b1fcaaf47ff0
hazel789 <xinjie.liau@gmail.com>
2022-01-18 01:09:41 +0800

    updated landingpage and app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import { Button } from "@chakra-ui/react";
+
 import "./App.css";
 import HotelDetails from "./components/HotelDetails";
 import Login from "./components/Login";
+import LandingPage from "./components/LandingPage";
 
 function App() {
+
+  //search input useStates
+  const [location, setLocation] = useState('')
+  const [inDate, setInDate] = useState('')
+  const [outDate, setOutDate] = useState('')
+  const [roomType, setRoomType] = useState('')
+
   // eslint-disable-next-line -- this is to "get rid" of the compilation warning that the state was assigned but never used.
   const [userFeedback, setUserFeedback] = useState("");
   // eslint-disable-next-line
@@ -29,6 +38,9 @@ function App() {
           userInfo={userInfo}
           setLoggedIn={setLoggedIn}
         />
+      </Route>
+      <Route exact path="/">
+        <LandingPage setLocation={setLocation} setInDate={setInDate} setOutDate={setOutDate} setRoomType={setRoomType} />
       </Route>
       <Route exact path="/hotel/:hotelId">
         <HotelDetails />
---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage = (props) => {
+    
+    const handleOnClick = (event) => {
+        event.preventDefault();
+
+        props.setSearchInput(keyword)
+        console.log("clicked")
+        props.setFetching(true)
+    }
+
+    const handleOnChange = (event) => {
+        setKeyword(event.target.value)
+    }
+
+    return (
+        <header>
+            <div className='header'>
+                <h1 className="header-title">Welcome!</h1>
+                <h2 className="header-title2">Explore millions of movies with us.</h2>
+
+                    <form>
+                    <input value={keyword} htmlFor="search" placeholder="Search for movies" className="searchBar" onChange={handleOnChange}></input>
+                    <input type="submit" id='searchButton' onClick={handleOnClick}></input>
+                    </form>
+        
+            </div>
+                </header>
+    )
+}
+
+export default LandingPage;
