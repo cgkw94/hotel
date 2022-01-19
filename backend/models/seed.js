6db5b72f8e2864864097c006f6018bc180069bbd
hazel789 <xinjie.liau@gmail.com>
2022-01-19 11:30:20 +0800

    seed

--- backend/models/seed.js
+++ backend/models/seed.js
@@ -580,8 +580,8 @@ module.exports = [
     ],
     userStayed: ["Jasmin", "Cheryl", "Lee"],
     rooms: [
-      { inDate: "02/02/21", outDate: "05/02/21", roomType: "Deluxe" },
-      { inDate: "03/03/21", outDate: "05/03/21", roomType: "Grand Deluxe" },
+      { inDate: "02/02/22", outDate: "05/02/22", roomType: "Deluxe" },
+      { inDate: "09/03/22", outDate: "10/03/22", roomType: "Grand Deluxe" },
     ],
     roomsGeneral: [
       {
