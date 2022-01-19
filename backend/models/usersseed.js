ec66e257df0220674b6df7c8c9fd241a5935fde8
ranie <ranieseah@gmail.com>
2022-01-19 10:24:51 +0800

    update

--- backend/models/usersseed.js
+++ backend/models/usersseed.js
@@ -12,4 +12,11 @@ module.exports = [
     passwordHash:
       "$2b$12$T23m0mvMxml.oTzSuqTpkuRV.UXHI4dNzf9AEaXO36JNuX5P3D7Fe", // password is getpassword
   },
+  {
+    username: "colin",
+    email: "colin@gmail.com",
+    passwordHash:
+      "$2b$12$osYd0ca9k1Rdwchnv8UwX.C.9zTPK6tchjDH6T7gsklKY4B9F6biS", // password is 1234
+    hotelStayed: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 38, 30],
+  },
 ];
